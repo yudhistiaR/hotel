4ead455221c082f689cd50105bf42f34ac3dcd2c
yudhistiaR <Yudhistia110@gmail.com>
2024-10-22 13:14:34 +0800

    aku tadi merubah file home

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full h-full bg-red-500 text-2xl">
-      <h1>Makan nasi iwak karing</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
+      <h1>Makan nasi lagii</h1>
     </div>
   );
 }
