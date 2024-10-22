eb973debffcca0834d024572d34b020b436a668e
yudhistiaR <Yudhistia110@gmail.com>
2024-10-22 13:07:49 +0800

    testing coyyy

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full h-full bg-red-500 text-2xl">
-      <h1>Maka ikan</h1>
-      <h1>Maka ikan</h1>
-      <h1>Maka ikan</h1>
-      <h1>Maka ikan</h1>
-      <h1>Maka ikan</h1>
-      <h1>Maka ikan</h1>
-      <h1>Maka ikan</h1>
+      <h1>Makan nasi iwak karing</h1>
     </div>
   );
 }
